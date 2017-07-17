79ca7111665fb950525254885dd28e3afa933ad4
Andrew Phillips <theasp@gmail.com>
2017-07-17 16:14:08 -0300

    Add build stuff

--- project.clj
+++ project.clj
@@ -6,8 +6,27 @@
                  [com.taoensso/timbre "4.10.0"]
                  [macchiato/core "0.1.8"]]
   :jvm-opts ^:replace ["-Xmx1g" "-server"]
-  :plugins [[lein-npm "0.6.2"]]
+  :plugins [[lein-npm "0.6.2"]
+            [lein-codox "0.10.2"]]
   :npm {:dependencies [[source-map-support "0.4.0"]]}
   :source-paths ["src" "target/classes"]
   :clean-targets ["out" "release"]
-  :target-path "target")
+  :target-path "target"
+
+  :deploy-repositories [["clojars" {:username :env/clojars_username
+                                    :password :env/clojars_password}]]
+
+  :hooks [leiningen.cljsbuild]
+
+  :codox {:language :clojurescript}
+
+  :prep-tasks ["compile" ["cljsbuild" "once"]]
+
+  :release-tasks [["vcs" "assert-committed"]
+                  ["change" "version" "leiningen.release/bump-version" "release"]
+                  ["vcs" "commit"]
+                  ["vcs" "tag" "v"]
+                  ["deploy" "clojars"]
+                  ["change" "version" "leiningen.release/bump-version"]
+                  ["vcs" "commit"]
+                  ["vcs" "push"]])
